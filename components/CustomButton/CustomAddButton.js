50f0054802df1a84b05a7cc18719d82504900070
Nikki Tran <nikkielizatran@gmail.com>
2022-12-01 10:17:23 +0700

    change add button to MUI

--- components/CustomButton/CustomAddButton.js
+++ components/CustomButton/CustomAddButton.js
@@ -1,32 +1,31 @@
 import { Pressable, StyleSheet, Text } from 'react-native'
 import { Icon } from 'react-native-elements'
+import { IconButton } from 'react-native-paper'
 
 
-const CustomAddButton = ( {onPress, label, disabled} ) => {
+const CustomAddButton = ({ onPress, label, disabled }) => {
   return (
     <Pressable style={styles.container}>
-      <Icon 
-        name='plus-circle'
-        type='font-awesome'
-        style={styles.icon}
+      <IconButton
+        icon="plus-circle"
         color='#1EA124'
+        size={25}
         onPress={onPress}
         disabled={disabled}
       />
-      <Text style={styles.text}>{label}</Text>
+      <Text>{label}</Text>
     </Pressable>
   )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flexDirection: 'row',
-        flex: 1,
-        paddingVertical: 10, 
-    },
-    text: {
-        marginLeft: 10
-    }
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    flex: 1,
+    paddingVertical: 5,
+  },
+
 })
 
 export default CustomAddButton;
